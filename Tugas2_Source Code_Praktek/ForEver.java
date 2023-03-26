b2621d3d4fe90ed19f1fb8a79f01667c37965d6c
HabibahBasri <127615548+HabibahBasri@users.noreply.github.com>
2023-03-26 20:33:20 +0800

    Add files via upload

---
+++ Tugas2_Source Code_Praktek/ForEver.java
@@ -0,0 +1,20 @@
+/* Habiba Basri
+ * 13020210264
+ * B1
+ * Kamis, 23 Maret
+ */
+
+package forever;
+public class ForEver {
+/**
+* @param args
+*/
+public static void main(String[] args) {
+// TODO Auto-generated method stub
+/* Program */
+System.out.println("Program akan looping, akhiri dengan ^c");
+while (true)
+{ System.out.print ("Print satu baris.....\n");
+    }
+    }
+}
